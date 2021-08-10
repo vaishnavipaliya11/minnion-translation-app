c11351b17fe1637469e513d51d85554773168948
vaishnavi paliya <81965405+vaishnavipaliya11@users.noreply.github.com>
2021-08-10 08:57:14 +0530

    input done

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-alert("working")
+var translateBtn= document.querySelector("#btn-translate")
+var txtInput = document.querySelector("#txt-input")
+
+function handleBtnClick(){
+  console.log("clicked")
+  console.log(txtInput.value);
+}
+translateBtn.addEventListener("click",handleBtnClick)
