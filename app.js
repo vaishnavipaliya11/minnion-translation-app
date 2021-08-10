42cf4ac7277b73ae28982a83394dde18e317f383
vaishnavi paliya <81965405+vaishnavipaliya11@users.noreply.github.com>
2021-08-10 09:03:25 +0530

    output div checked

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var translateBtn= document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-input")
+var output= document.querySelector(".output")
 
 function handleBtnClick(){
   console.log("clicked")
   console.log(txtInput.value);
+  output.innerText= "hello"
 }
 translateBtn.addEventListener("click",handleBtnClick)
