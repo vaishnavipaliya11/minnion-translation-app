c355e8437abce4ced11664d76a977b6d801dbd88
vaishnavi paliya <81965405+vaishnavipaliya11@users.noreply.github.com>
2021-08-10 09:54:59 +0530

    fetched with mock server

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ var translateBtn= document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-input")
 var output= document.querySelector(".output")
 
-function handleBtnClick(){
-  console.log("clicked")
-  console.log(txtInput.value);
-  output.innerText= "hello"
+var serverUrl= "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getServerUrl(text){
+  return serverUrl +"?"+"text="+text
 }
+
+
+function handleBtnClick(userInput){
+var userInput= txtInput.value;
+
+fetch(getServerUrl(userInput))
+.then(response => response.json())
+.then(json => console.log(json))
+}
+
 translateBtn.addEventListener("click",handleBtnClick)
